107990f7e92f9b0040033036f26bc43b228f0603
William McLane <dukeoftinmen@gmail.com>
2017-10-18 01:15:44 +0000

    users done

--- lib/kele.rb
+++ lib/kele.rb
@@ -14,4 +14,9 @@ class Kele
     raise "Invalid Email or Password. Try Again." if @user_auth_token.nil?
 
   end
+  
+  def get_me
+    response = self.class.get('/users/me', headers: { "authorization" => @user_auth_token })
+    @user = JSON.parse(response.body)
+  end
 end
