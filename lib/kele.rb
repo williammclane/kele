7040eb803f908db740f8450074cadfde6b2560ff
William McLane <dukeoftinmen@gmail.com>
2017-10-18 01:12:39 +0000

    initialize done

---
+++ lib/kele.rb
@@ -0,0 +1,17 @@
+require 'httparty'
+
+class Kele
+  include HTTParty
+
+  base_uri 'https://www.bloc.io/api/v1'
+
+  def initialize(email, password)
+    post_response = self.class.post('/sessions', body: {
+        email: email,
+        password: password
+      })
+    @user_auth_token = post_response['auth_token']
+    raise "Invalid Email or Password. Try Again." if @user_auth_token.nil?
+
+  end
+end
