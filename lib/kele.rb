d6ca68853d47d94b9a4e7d9df8731a8e77996332
William McLane <dukeoftinmen@gmail.com>
2017-10-18 01:39:18 +0000

    messaging done

--- lib/kele.rb
+++ lib/kele.rb
@@ -23,9 +23,30 @@ class Kele
   end
   
   def get_mentor_availability(mentor_id)
-    response = self.class.get('/mentors/' + mentor_id.to_s + '/student_availability', headers: {"authorization" => @user_auth_token })
+    response = self.class.get('/mentors/' + mentor_id.to_s + '/student_availability', headers: { "authorization" => @user_auth_token })
     body = JSON.parse(response.body)
     mentor_availability = []
     body.each { |time_slot| mentor_availability << time_slot if time_slot['booked'] == nil }
+  end
+  
+  def get_messages(page = nil)
+    if page != nil
+      response = self.class.get("/message_threads", body: {"page": page}, headers: { "authorization" => @user_auth_token })
+    else
+      response = self.class.get("/message_threads", headers: { "authorization" => @user_auth_token })
+    end
+
+    @message_threads = JSON.parse response.body
+  end
+
+  def create_message(sender, recipient_id, subject, stripped_text)
+    response = self.class.post('/messages',
+    body: {
+      "sender": sender,
+      "recipient_id": recipient_id,
+      "subject": subject,
+      "stripped-text": stripped_text
+    },
+    headers: { "authorization" => @user_auth_token })
   end
 end
